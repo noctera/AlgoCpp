b82580d30ae5be5d3663410ea3e0bd41b56270c7
noctera <julian.jaksch@gmx.de>
2021-12-17 23:30:18 +0100

    refactor: using iterators for bubblesort

--- include/AlgoCpp/Algorithms/Sorting/BubbleSort.hpp
+++ include/AlgoCpp/Algorithms/Sorting/BubbleSort.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 
 namespace algocpp {
@@ -14,9 +15,9 @@ void bubbleSort(T& input) {
     // count if changes were made in an iteration to indicate if everything is in right order
     int changesPerIteration = 0;
     while (true) {
-        for (int i = 0; i < input.size() - 1; ++i) {
-            if (input[i] > input[i + 1]) {
-                std::swap(input[i], input[i + 1]);
+        for (auto it = input.begin(); it != input.end() - 1; ++it) {
+            if (*it > *std::next(it)) {
+                std::iter_swap(it, std::next(it));
                 ++changesPerIteration;
             }
         }
